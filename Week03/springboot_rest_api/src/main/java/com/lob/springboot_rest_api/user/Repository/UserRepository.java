477a65d11ecf3d4ae2958d673ee34db58128dd6c
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-09-09 12:35:37 +0900

    기존 프로젝트 제거

--- Week03/springboot_rest_api/src/main/java/com/lob/springboot_rest_api/user/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lob.springboot_rest_api.user.Repository;
-
-import com.lob.springboot_rest_api.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String>{
-}
--- Week03/springboot_rest_api/src/main/java/com/lob/springboot_rest_api/user/dto/ApiResource.java
+++
@@ -1,4 +0,0 @@
-package com.lob.springboot_rest_api.user.dto;
-
-public class ApiResource {
-}
