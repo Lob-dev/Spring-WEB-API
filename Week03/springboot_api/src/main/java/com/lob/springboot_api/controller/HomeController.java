3c6f332819857f0ce3f985ed4148706a8447a704
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-09-09 18:03:52 +0900

    no message

--- Week03/springboot_api/src/main/java/com/lob/springboot_api/controller/HomeController.java
+++
@@ -1,9 +0,0 @@
-package com.lob.springboot_api.controller;
-
-public class HomeController {
-    // 인덱스 페이지 컨트롤러
-    // request code = GI / code_explain = GETINDEX
-    // requestID Generate value +1 / userID guests / requestCode = requestcode.requestCode / createDate = dateTime
-
-    // requestService.requestForGetIndexPage("guests","GI") Logic을 진행할 때 DATE 생성 (datetime format YYMMDDhhmm)
-}
