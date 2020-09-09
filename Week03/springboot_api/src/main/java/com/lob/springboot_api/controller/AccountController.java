6a37bd9cf9687a961d6da4276ae879c9294ca825
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-09-09 12:36:04 +0900

    새 프로젝트 생성

---
+++ Week03/springboot_api/src/main/java/com/lob/springboot_api/controller/AccountController.java
@@ -0,0 +1,11 @@
+package com.lob.springboot_api.controller;
+
+public class AccountController {
+    // 로그인 페이지 컨트롤러
+    // request / code = L / code_explain = LOGIN
+    // user / userID = userID / HR_ORGAN = HR_ORGAN / USERNAME = USERNAME
+    // requestID / Generate value +1 / userID user.userID / requestCode = requestcode.requestCode / createDate = dateTime
+
+    // AccountService.join(user)
+    // requestService.requestForAccount(UserDTO.getUserID,"L")
+}
---
+++ Week03/springboot_api/src/main/java/com/lob/springboot_api/controller/ApiCotroller.java
@@ -0,0 +1,9 @@
+package com.lob.springboot_api.controller;
+
+public class ApiCotroller {
+    // API 요청 컨트롤러
+    // request code = WB / code_explain = WRITEBOARD
+    // requestID Generate value +1 / userID userID / requestCode = requestcode.requestCode / createDate = dateTime
+
+    // requestService.requestForWriteBoard(UserDTO.getUserID,"WB") Logic을 진행할 때 DATE 생성 (datetime format YYMMDDhhmm)
+}
